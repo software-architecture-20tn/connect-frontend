60ca7aa61c393db02d0256ff4739df398cde23b3
nhu.pham <nhu.pham@adminkatalons-MacBook-Air.local>
2023-10-28 18:39:05 +0700

    structure of the app

---
+++ src/Pages/LogIn/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Login.scss'
+function Login() {
+  return (
+    <div className='login-page'>
+        <h1>Login</h1>
+    </div>
+  )
+}
+
+export default Login
