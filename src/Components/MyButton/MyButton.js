cde9497423e807487ae9485cb3fc94f201bf3fca
Nguyen Van Loc <76842571+vanloc1808@users.noreply.github.com>
2023-11-02 23:14:56 +0700

    Revert "Revert "Chat 16""

--- src/Components/MyButton/MyButton.js
+++ src/Components/MyButton/MyButton.js
@@ -5,9 +5,17 @@ function MyButton({ className, text, type, variant = "contained", ...props }) {
   return (
     <div>
       <Button
-        className={`${className} custom-button`}
+        className={`custom-button ${className}`}
         variant={variant}
         type={type}
+        color="primary"
+        sx={{
+          backgroundColor: "var(--button-color)",
+          borderRadius: "50px",
+          "&:hover": {
+            backgroundColor: "var(--secondary-button-color)",
+          },
+        }}
         {...props}
       >
         {text}
