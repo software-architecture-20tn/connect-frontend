46f1ef8fe4b4deda3ad8688da0407c0d1fd3cba4
Nguyen Van Loc <vanloc1808@gmail.com>
2023-12-11 20:52:29 +0700

    CHAT-36 Implement screen for password forgot

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,10 +1,12 @@
 import Home from "../Pages/Home";
 import Login from "../Pages/LogIn";
 import Signup from "../Pages/Signup";
+import PasswordForgot from "../Pages/PasswordForgot";
 
 const publicRoutes = [
   { path: "/login", component: Login },
   { path: "/register", component: Signup },
+  { path: "/forgot-password", component: PasswordForgot },
 ];
 
 const privateRoutes = [{ path: "/", component: Home }];
