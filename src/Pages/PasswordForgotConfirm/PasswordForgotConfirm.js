3f51e2117c8c02c1794a90dcef7834577ccd2784
Nguyen Bao Tin <legendarystudent2402@gmail.com>
2024-02-03 18:25:54 +0700

    fix message

--- src/Pages/PasswordForgotConfirm/PasswordForgotConfirm.js
+++ src/Pages/PasswordForgotConfirm/PasswordForgotConfirm.js
@@ -99,15 +99,10 @@ function PasswordForgotConfirm() {
 
         <MyButton text="Confirm Reset" type="submit" />
 
-        {showSuccessMessage ? (
+        {showSuccessMessage && (
           <div className="password-forgot-confirm__form__success-message">
             <p>Password reset successfully confirmed.</p>
             <p>You can now log in with your new password.</p>
-          </div>
-        ) : (
-          <div className="password-forgot-confirm__form__error-message">
-            <p>Something went wrong.</p>
-            <p>Please try again.</p>
           </div>
         )}
       </form>
